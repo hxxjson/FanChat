66fdedfb64a3e0cdbd7f73e64534e2dd7195187f
hxx <hxx@tqscsoft.com>
2016-12-06 16:15:22 +0800

    测试

--- app/src/main/java/com/itheima/leon/qqdemo/ui/activity/TestActivity.java
+++ app/src/main/java/com/itheima/leon/qqdemo/ui/activity/TestActivity.java
@@ -1,7 +1,9 @@
 package com.itheima.leon.qqdemo.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.itheima.leon.qqdemo.R;
 
 public class TestActivity extends AppCompatActivity {
 
